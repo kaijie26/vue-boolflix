23755c534f28b8f00205e2598fb82fb400fc5ac0
kaijie26 <chenkaijie26@gmail.com>
2022-07-12 13:06:38 +0200

    create basic files components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<!-- HTML -->
+<template>
+    <div>
+        
+        
+    </div>
+
+</template>
+<!-- JAVASCRIPT -->
+
+<script>
+export default {
+    name:'HeaderComponent'
+
+    
+}
+</script>
+
+<!-- CSS -->
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<!-- HTML -->
+<template>
+    <div>
+        
+    </div>
+
+    
+</template>
+
+<!-- JAVASCRIPT -->
+<script>
+export default {
+    name: 'MainComponent'
+    
+}
+</script>
+
+<!-- CSS -->
+<style lang="scss" scoped>
+
+</style>
