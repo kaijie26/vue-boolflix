f0c373de360fe6cb9937fd821b22db9de9798e85
kaijie26 <chenkaijie26@gmail.com>
2022-07-14 18:18:14 +0200

    milestone 4 done

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -2,43 +2,52 @@
 <template>
         <!-- Single Film and SerieTv -->
         <li class="card" >
-            <!-- Poster -->
-            <div>
-                <img v-if="item.poster_path" class="poster" :src="`https://image.tmdb.org/t/p/w342/${item.poster_path}`" alt="">
-                <img v-else class="poster" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt="">
-            </div>
+            <div class="cover">
+                <!-- Poster -->
+                <div class="poster-container">
+                    <img v-if="item.poster_path" class="poster" :src="`https://image.tmdb.org/t/p/w342/${item.poster_path}`" alt="">
+                    <img v-else class="poster" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt="">
+                </div>
 
-            <!-- Title -->
-            <div>
-                Titolo:{{ item.title? item.title : item.name }}
-            </div>
+                <!-- Info-Container -->
+                <div class="info-container">
+                    <!-- Title -->
+                    <div>
+                        Titolo:{{ item.title? item.title : item.name }}
+                    </div>
 
-            <!-- Original-Title -->
-            <div>
-                Titolo Originale:{{ item.original_title? item.original_title : item.original_name }}
-            </div>
+                    <!-- Original-Title -->
+                    <div>
+                        Titolo Originale:{{ item.original_title? item.original_title : item.original_name }}
+                    </div>
 
-            <!-- Language -->
-            <div>Lingua:
-                <img v-if="flags.includes(item.original_language)" :src="require(`../assets/img/flag-${item.original_language}.png`)" class="flags" :alt="item.original_language" >
-                <span v-else>{{ item.original_language }}</span> 
-                
-            </div>
+                    <!-- Language -->
+                    <div>Lingua:
+                        <img v-if="flags.includes(item.original_language)" :src="require(`../assets/img/flag-${item.original_language}.png`)" class="flags" :alt="item.original_language" >
+                        <span v-else>{{ item.original_language }}</span> 
+                        
+                    </div>
 
-            <!-- Vote -->
-            <div>
-                Voto:{{ item.vote_average }}
-            </div>
-            <div> Voto Arrotondato: {{ getStar(item.vote_average) }}
-                <span class="colored-star">
-                    <i v-for="n in getStar(item.vote_average)" :key="n" class="fa-solid fa-star"></i>
-                </span>
-                <span class="empty-star">
-                    <i v-for="n in 5 - getStar(item.vote_average)" :key="n" class="fa-regular fa-star"></i>
-                </span>
+                    <!-- Vote -->
+                    <div>
+                        Voto:{{ item.vote_average }}
+                        <span class="colored-star">
+                            <i v-for="n in getStar(item.vote_average)" :key="n" class="fa-solid fa-star"></i>
+                        </span>
+                        <span class="empty-star">
+                            <i v-for="n in 5 - getStar(item.vote_average)" :key="n" class="fa-regular fa-star"></i>
+                        </span>
+                    </div>
+
+                    <!-- OverView -->
+                    <div>
+                        OverView:{{item.overview}}
+                    </div>
+
+                </div>
+
             </div>
             
-
         </li>
 
 </template>
@@ -80,7 +89,6 @@ export default {
 
 <style lang="scss" scoped>
 .card{
-    // background-color: coral;
     max-width: 250px;
     margin: 30px;
     font-size: 13px;
@@ -100,6 +108,27 @@ export default {
 .colored-star{
     color: yellow;
     
+}
+
+.cover{
+    position: relative;
+}
+
+.info-container{
+    position: absolute;
+    bottom: 0%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow-x: auto;
+    background: black;
+    line-height: 20px;
+    padding: 10px;
+    display: none;
+}
+
+.cover:hover .info-container{
+    display: block;
 }
 
 </style>
